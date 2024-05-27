d261232987dc58b86f62f9ab957c15b77994d91a
Or <orshkuri2000@gmail.com>
2024-05-27 11:09:22 +0300

    Added datasets

---
+++ experiments/perliminary_results/test.py
@@ -0,0 +1,36 @@
+import random
+
+import pandas
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+# data = {
+#     'first': [1, 2, 3],
+#     'second': [4, 5, 6],
+#     'third': [0, 1, 2]
+# }
+#
+# data = pandas.DataFrame(data)
+# print(data)
+# print('-------------')
+# data = data.iloc[:, :-1]
+# print(data)
+
+# print(df)
+# df.iloc[:, 1] = 0
+# print(df)
+
+# logistic_reg = LogisticRegression()
+#
+# X = data[['first', 'second']]
+# y = data[['third']]
+# logistic_reg.fit(X, y)
+#
+# y_pred = logistic_reg.predict_proba(X)
+# print(y)
+# print(type(y))
+# print(y_pred)
+# print(type(y_pred))
+# print(y_pred.shape)
+
+
---
+++ scripts/test_file.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+df = pd.read_csv("../datasets/Heart Disease/heart_statlog_cleveland_hungary_final.csv")
+print(df)
